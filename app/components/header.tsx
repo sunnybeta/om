c4e355b2f87b72bb496182d699fa236c85260164
Sunny <104438126+sunnybeta@users.noreply.github.com>
2023-12-04 15:02:57 +0530

    Feat: Next JS

---
+++ app/components/header.tsx
@@ -0,0 +1,15 @@
+import '../styles/header.css';
+
+const Header = () => {
+	return (
+		<header>
+			<section className='strip'>
+				<span className='mantra'>
+					ॐ भूर् भुवः स्वः। तत् सवितुर्वरेण्यं। भर्गो देवस्य धीमहि। धियो यो नः प्रचोदयात् ॥
+				</span>
+			</section>
+		</header>
+	);
+};
+
+export default Header;
